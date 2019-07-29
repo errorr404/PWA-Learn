8a19813245b85fcff74324b9801f7f2fff1fb6d6
codebrahma <dkbishwash@gmail.com>
2019-07-29 15:13:52 +0530

    feat: Added routes in cache
    Added routes in cache

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,48 @@
-self.addEventListener('install', e => {
-  console.log('Installing servie Worker--->', e);
-  e.waitUntill(
-    caches.open('static').then(cache => cache.add('/src/js/app.js'))
+
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static')
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll([
+          '/index.html',
+          '/src/js/app.js',
+          '/src/js/feed.js',
+          '/src/js/promise.js',
+          '/src/js/fetch.js',
+          '/src/js/material.min.js',
+          '/src/css/app.css',
+          '/src/css/feed.css',
+          '/src/images/main-image.jpg',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ]);
+      })
+  )
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log('fetch--->',event.request);
+  event.respondWith(
+    caches.open('static').then((cache) => {
+      return cache.match(event.request)
+        .then(function(response) {
+          if(response) {
+            return response;
+          }
+          return fetch(event.request).then(res => {
+            return res;
+          }); 
+        }).catch((err) => {
+          console.log({ err });
+      })
+    })
   );
-});
-
-self.addEventListener('activate', e => {
-  console.log('activating a servie Worker---->', e);
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  // console.log(' fetch is triggered---->', e);
 });
