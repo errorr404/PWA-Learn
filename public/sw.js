3314977c109664dfd1489787ba6cb1498ca5450a
codebrahma <dkbishwash@gmail.com>
2019-07-29 18:00:37 +0530

    feat: On demand caching
    On demand caching--> when we press the button it will save the data to cache

--- public/sw.js
+++ public/sw.js
@@ -1,64 +1,4 @@
-// var CACHE_STAIC_NAME = 'static-v4';
-// var CACHE_DYNAMIC_NAME = 'dynamic-v2';
-
-// self.addEventListener('install', function(event) {
-//   console.log('[Service Worker] Installing Service Worker ...', event);
-//   event.waitUntil(
-//     caches.open(CACHE_STAIC_NAME).then(function(cache) {
-//       console.log('[Service Worker] Precaching App Shell');
-//       cache.addAll([
-//         '/index.html',
-//         '/src/js/app.js',
-//         '/src/js/feed.js',
-//         '/src/js/promise.js',
-//         '/src/js/fetch.js',
-//         '/src/js/material.min.js',
-//         '/src/css/app.css',
-//         '/src/css/feed.css',
-//         '/src/images/main-image.jpg',
-//         'https://fonts.googleapis.com/css?family=Roboto:400,700',
-//         'https://fonts.googleapis.com/icon?family=Material+Icons',
-//         'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-//       ]);
-//     })
-//   );
-// });
-
-// self.addEventListener('activate', function(event) {
-//   console.log('[Service Worker] Activating Service Worker ....', event);
-//   //old cache cleanup
-//   event.waitUntil(
-//     caches.keys().then(keyList => {
-//       return Promise.all(
-//         keyList.map(key => {
-//           if (key !== CACHE_STAIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-//             console.log('servieWorker removing old cache', key);
-//             return caches.delete(key);
-//           }
-//         })
-//       );
-//     })
-//   );
-//   event.waitUntil(self.clients.claim()); 
-// });
-
-// self.addEventListener('fetch', function(event) {
-//   console.log('fetch--->', event.request);
-//   event.respondWith(
-//     caches.match(event.request).then(response => {
-//       if (response) return response;
-//       return fetch(event.request).then(res => {
-//         return caches.open(CACHE_DYNAMIC_NAME).then(cache => {
-//           cache.put(event.request.url, res.clone());
-//           return res;
-//         });
-//       });
-//     })
-//   );
-// });
-
-
-var CACHE_STATIC_NAME = 'static-v5';
+var CACHE_STATIC_NAME = 'static-v7';
 var CACHE_DYNAMIC_NAME = 'dynamic-v3';
 
 self.addEventListener('install', function(event) {
@@ -113,7 +53,7 @@ self.addEventListener('fetch', function(event) {
             .then(function(res) {
               return caches.open(CACHE_DYNAMIC_NAME)
                 .then(function(cache) {
-                  cache.put(event.request.url, res.clone());
+                  // cache.put(event.request.url, res.clone());
                   return res;
                 })
             })
