769d7773865d4fed96fd63c2e30220bc1710c619
codebrahma <dkbishwash@gmail.com>
2019-07-29 12:31:32 +0530

    feat: Added new Card
    Added new Card

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,12 @@
 self.addEventListener('install', e => {
-  console.log('Installing servie Worker--->', e);
+  // console.log('Installing servie Worker--->', e);
 });
 
 self.addEventListener('activate', e => {
-  console.log('activating a servie Worker---->', e);
+  // console.log('activating a servie Worker---->', e);
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  console.log(' fetch is triggered---->', e);
+  // console.log(' fetch is triggered---->', e);
 });
